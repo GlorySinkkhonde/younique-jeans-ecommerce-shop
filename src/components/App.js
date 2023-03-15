f1e41f980f48939d53c95a3436355efe962a1938
Glory Sinkhonde <sinkhondeglory@gmail.com>
2023-03-15 20:59:50 +0200

    Setup with webpack

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello World ddddddddd</h1>
+                <h4>hi</h4>
+            </div>
+        )
+    }
+}
+
+export default App;
