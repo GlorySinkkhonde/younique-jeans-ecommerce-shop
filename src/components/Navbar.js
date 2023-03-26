4e1d0fe9580f72e6f22114ef216410a3ee079901
Glory Sinkhonde <sinkhondeglory@gmail.com>
2023-03-26 21:50:12 +0200

    Styled nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,23 +5,40 @@ import { FaShoppingCart } from 'react-icons/fa'
 import WebsiteLogo from '../assets/images/logo.png'
 
 function Navbar() {
+
+    function ToggleNav() {
+        console.log('hello')
+    }
+
   return (
+
     <nav className='nav'>
-        <ul className='nav__first-nav'>
-            <CustomLink to='/shop'>Shop</CustomLink>
-            <CustomLink to='/'>Home</CustomLink>
-            <CustomLink to='/about'>About</CustomLink>
-        </ul>
 
-        <Link to='/' className='site-logo'>
-            <img src={WebsiteLogo} alt='Younique Jeans Website Logo' />
-        </Link>
+        <div className='nav__primary-nav'>
 
-        <ul className='nav__second-nav'>
-            <CustomLink to='/contact'>Contact</CustomLink>
-            <CustomLink to='/your-account'>Your Account</CustomLink>
-            <CustomLink to='/cart'> <span className='cart-icon' ><FaShoppingCart /></span> <span className='item-total'>0</span> </CustomLink>
-        </ul>
+            <div className='nav__primary-nav-1'>
+                <ul className='nav__first-nav'>
+                <CustomLink to='/shop'>Shop</CustomLink>
+                <CustomLink to='/'>Home</CustomLink>
+                <CustomLink to='/about'>About</CustomLink>
+                </ul>
+
+                <Link to='/' className='site-logo'>
+                    <img src={WebsiteLogo} alt='Younique Jeans Website Logo' />
+                </Link>
+            </div>
+        
+            <ul className='nav__second-nav'>
+                <CustomLink to='/contact'>Contact</CustomLink>
+                <CustomLink to='/your-account'>Your Account</CustomLink>
+                <CustomLink to='/cart'> <span className='cart-icon' ><FaShoppingCart /></span> <span className='item-total'>0</span> </CustomLink>
+            </ul>
+        </div>
+        
+        <button className='nav-toggle' ria-label='open navigation' onClick={ToggleNav}>
+            <span className='hamburger'></span>
+        </button>
+
     </nav>
   )
 }
